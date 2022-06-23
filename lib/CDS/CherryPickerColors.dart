c4cef5d6602274a65f3d3c0069aaac22db0915ed
22sonamu <22sonamu@naver.com>
2022-06-23 19:53:04 +0900

    drop down menu

--- lib/CDS/CherryPickerColors.dart
+++ lib/CDS/CherryPickerColors.dart
@@ -2,5 +2,6 @@ import 'dart:ui';
 
 class CherryPickerColors{
   static const Color maincolor = const Color(0xff435CFA);
+  static const Color greycolor = const Color(0xff7E8590);
 
 }
--- lib/Login/SignupScreen.dart
+++ lib/Login/SignupScreen.dart
@@ -1,7 +1,8 @@
 import 'package:cherrypicker/CDS/CherryPickerButton.dart';
+import 'package:cherrypicker/CDS/CherryPickerColors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:dropdown_button2/dropdown_button2.dart';
 import '../main.dart';
 
 class SignupScreen extends StatefulWidget {
@@ -19,8 +20,8 @@ class _SignupScreenState extends State<SignupScreen> {
     final width = MediaQuery.of(context).size.width ;
     final h_percent = height/height_whole;
     final w_percent = width/ width_whole;
-    var selectedValue = 'SKT';
-    final items = ["SKT", "KT", "LG U+", "SKT 알뜰폰", "KT 알뜰폰", "LG 알뜰폰"];
+    String? selectedValue ;
+    List<String> items = ["SKT", "KT", "LG U+", "SKT 알뜰폰", "KT 알뜰폰", "LG 알뜰폰"];
 
     return Scaffold(
       appBar: AppBar(
@@ -28,87 +29,127 @@ class _SignupScreenState extends State<SignupScreen> {
       backgroundColor: Colors.white,
       leading: const Icon(Icons.arrow_back_ios, size : 20, color : Color(0xff7E8590)),
       centerTitle : true,),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children:[
-          Padding(
-            padding: EdgeInsets.only(top : 30 * h_percent, left : 25 * w_percent),
-            child: const Text("입력한 정보가 맞다면", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color : Colors.black),),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 25 * w_percent),
-            child: const Text("아래 확인 버튼을 눌러주세요", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color : Colors.black),),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top : 55 * h_percent,left: 25 * w_percent),
-            child: const Text("휴대폰 번호", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color : Colors.black),),
-          ),
-          Padding(
-            padding: EdgeInsets.only( left: 24 * w_percent),
-            child: SizedBox(
-              width: 312 * w_percent,
-              height: 30 * h_percent,
-              child: TextField(
-                decoration: InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color : Color(0xffDDE3EC)),
-                    borderRadius: BorderRadius.circular(8.0)
+      body: Container(
+        color : Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:[
+            Padding(
+              padding: EdgeInsets.only(top : 30 * h_percent, left : 25 * w_percent),
+              child: const Text("입력한 정보가 맞다면", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color : Colors.black),),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 25 * w_percent),
+              child: const Text("아래 확인 버튼을 눌러주세요", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color : Colors.black),),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top : 55 * h_percent,left: 25 * w_percent),
+              child: const Text("휴대폰 번호", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color : Colors.black),),
+            ),
+            Padding(
+              padding: EdgeInsets.only( left: 24 * w_percent),
+              child: SizedBox(
+                width: 312 * w_percent,
+                height: 30 * h_percent,
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color : Color(0xffDDE3EC)),
+                      borderRadius: BorderRadius.circular(8.0)
+                    ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color : Color(0xff435CFA)),
+                          borderRadius: BorderRadius.circular(8.0)
+                      )
                   ),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color : Color(0xff435CFA)),
-                        borderRadius: BorderRadius.circular(8.0)
-                    )
+
+
                 ),
-
-
               ),
             ),
-          ),
-          Padding(
-            padding:EdgeInsets.only(top : 16 * h_percent, left: 25 * w_percent),
-            child: const Text("통신사" , style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color : Colors.black),),
-          ),
-          Padding(
-            padding: EdgeInsets.only( left: 25 * w_percent),
-            child: SizedBox(
-              width: 312 * w_percent,
-              height: 30 * h_percent,
-              child: DropdownButton<String>(
-                value: selectedValue,
-                onChanged: (newValue){
-                  selectedValue = newValue!;
-                  setState(() {
+            Padding(
+              padding:EdgeInsets.only(top : 16 * h_percent, left: 25 * w_percent),
+              child: const Text("통신사" , style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color : Colors.black),),
+            ),
+            Padding(
+              padding: EdgeInsets.only( left: 25 * w_percent),
+              child: SizedBox(
+                width: 312 * w_percent,
+                height: 30 * h_percent,
+                child: DropdownButtonFormField(
+                  decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color : Color(0xffDDE3EC)),
+                          borderRadius: BorderRadius.circular(8.0)
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color : Color(0xff435CFA)),
+                          borderRadius: BorderRadius.circular(8.0)
+                      )
+                  ),
+                  icon: Center(child: Icon(Icons.keyboard_arrow_down_rounded, color : CherryPickerColors.greycolor)),
+                  isDense: true,
 
+                  items: items.map((item) =>
+                    DropdownMenuItem<String>(
 
-                  });
-                },
-
-                items: items
-                    .map<DropdownMenuItem<String>>(
-                        (String value) => DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
+                      value: item,
+                      child: Text(
+                        item,
+                        style: const TextStyle(
+                          fontSize: 13,
+                        ),
+                      ),
                     ))
                     .toList(),
+                  value: selectedValue,
+                  onSaved: (value) {
 
-                // add extra sugar..
-                icon: Icon(Icons.keyboard_arrow_down_rounded),
-                iconSize: 42,
-                underline: SizedBox(),
+                    setState(() {
+
+                      selectedValue = value as String;
+                    });
+                  }, onChanged: (String? value) {
+                    setState(() {
+
+                      selectedValue = value as String;
+                    });
+                  },
+
+
+                )
+
               ),
-
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top : 16 * h_percent, left: 25 * w_percent),
-            child: const Text("주민등록번호", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color : Colors.black),),
-          ),
-          Row(
+            Padding(
+              padding: EdgeInsets.only(top : 16 * h_percent, left: 25 * w_percent),
+              child: const Text("주민등록번호", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color : Colors.black),),
+            ),
+            Row(
 
-            children: [
-              Padding(
-                padding: EdgeInsets.only( left: 24 * w_percent),
-                child: SizedBox(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only( left: 24 * w_percent),
+                  child: SizedBox(
+                    width: 120 * w_percent,
+                    height: 30 * h_percent,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color : Color(0xffDDE3EC)),
+                              borderRadius: BorderRadius.circular(8.0)
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color : Color(0xff435CFA)),
+                              borderRadius: BorderRadius.circular(8.0)
+                          )
+                      ),
+
+                    ),
+                  ),
+                ),
+                const Text("-  "),
+                SizedBox(
                   width: 120 * w_percent,
                   height: 30 * h_percent,
                   child: TextField(
@@ -125,10 +166,17 @@ class _SignupScreenState extends State<SignupScreen> {
 
                   ),
                 ),
-              ),
-              const Text("-  "),
-              SizedBox(
-                width: 120 * w_percent,
+
+              ],
+            ),
+            Padding(
+              padding:EdgeInsets.only(top : 16 * h_percent, left: 25 * w_percent),
+              child: const Text("이름", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color : Colors.black),),
+            ),
+            Padding(
+              padding: EdgeInsets.only( left: 24 * w_percent),
+              child: SizedBox(
+                width: 312 * w_percent,
                 height: 30 * h_percent,
                 child: TextField(
                   decoration: InputDecoration(
@@ -144,46 +192,21 @@ class _SignupScreenState extends State<SignupScreen> {
 
                 ),
               ),
-
-            ],
-          ),
-          Padding(
-            padding:EdgeInsets.only(top : 16 * h_percent, left: 25 * w_percent),
-            child: const Text("이름", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color : Colors.black),),
-          ),
-          Padding(
-            padding: EdgeInsets.only( left: 24 * w_percent),
-            child: SizedBox(
-              width: 312 * w_percent,
-              height: 30 * h_percent,
-              child: TextField(
-                decoration: InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color : Color(0xffDDE3EC)),
-                        borderRadius: BorderRadius.circular(8.0)
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color : Color(0xff435CFA)),
-                        borderRadius: BorderRadius.circular(8.0)
-                    )
-                ),
-
+            ),
+            Padding(
+              padding: EdgeInsets.only( top: 20 * h_percent),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CherryPickerButton.MainButton(h_percent * 50, h_percent * 312, "확인")
+                ],
               ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only( top: 20 * h_percent),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CherryPickerButton.MainButton(h_percent * 50, h_percent * 312, "확인")
-              ],
-            ),
-          )
+            )
 
 
 
-        ],
+          ],
+        ),
       ),
     );
   }
